14c0f0e9be61ad027dd8d689bdbd3455e987da09
AleksandrKompanetc <moneymacker7@gmail.com>
2024-08-13 23:39:28 +0300

    added main files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h3>htmllessons (JS Version)</h3>
+      <input defaultValue='Max' placeholder='Enter name' />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/AppTs.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import './App.css';
+
+const AppTs: FC = () => {
+  return <div>TypeScript Version</div>
+}
+
+export default AppTs;
