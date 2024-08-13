48b2374ee9b6870c3ab7831879a77c1d6e92e20d
AleksandrKompanetc <moneymacker7@gmail.com>
2024-08-13 23:59:52 +0300

    added useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
+  const [data, setData] = useState('');
   return (
     <div>
       <h3>htmllessons (JS Version)</h3>
-      <input defaultValue='Max' placeholder='Enter name' />
+      <input 
+        value={data.name} 
+        onChange={e => setData({...data, name: e.target.value})} 
+        defaultValue='Max' 
+        placeholder='Enter name' 
+      />
+      <div>
+        <b>Value:</b>
+        {data.name}
+      </div>
     </div>
   )
 }
