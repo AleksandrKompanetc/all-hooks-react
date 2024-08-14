5c4a809cd8c9db0b4098bda2b2d130e88ce74ff0
AleksandrKompanetc <moneymacker7@gmail.com>
2024-08-14 15:48:36 +0300

    added useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import React, {useState, useCallback} from 'react'
+import React, {useReducer} from 'react'
 import { useName } from './useName';
+import { initialState, reducer } from './reducer';
+import { COMPLETE, UNCOMPLETE } from './reducer';
 
 function App() {
-  const [name, setName] = useName()
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   return (
     <div>
       <h3>htmllessons (JS Version)</h3>
+      {state.map(todo => (
+        <button 
+          key={todo.name} 
+          style={{display: 'flex', alignItems: 'center'}}
+          onClick={() => dispatch({type: COMPLETE, payload: todo.name})}
+        >
+          <div style={{backgroundColor: todo.isCompleted ? 'green' : 'gray', width: 15, height: 15, marginRight: 5}} />
+          <div>{todo.name}</div>
+        </button>
+      ))}
       <input 
-        value={name}
         placeholder='Enter name' 
       />
     </div>
